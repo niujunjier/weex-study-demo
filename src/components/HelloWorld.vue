18bec1f27350787f2af6e3c0b1bbb9683e4baee4
njj牛俊杰 <niujj@ctrip.com>
2018-02-24 18:54:32 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,3 @@
+<template>
+  <text class="message">let's go</text>
+</template>
---
+++ src/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper" @click="update">
+    <image :src="logoUrl" class="logo" />
+    <text class="title">Hello {{target}}</text>
+    <text class="desc">Now, let's use vue to build your weex app.</text>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: {
+      logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
+      target: 'World'
+    },
+    methods: {
+      update: function (e) {
+        this.target = 'Weex'
+        console.log('target:', this.target)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
